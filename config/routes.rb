6a448d46fa5b35512f1d7f3f4d614054748a92c2
yuva k <yuvarajan1989@gmail.com>
2020-10-19 20:01:18 +0800

    finished index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: "tasks#index"
+  get '/tasks', to: "tasks#index"
+  #read one task
+  get '/tasks/:id', to: "tasks#show"
+  #create a task
+  get '/tasks/new', to: "tasks#new"
+  post '/tasks', to: "tasks#create"
+
+  #update a task
+  get '/tasks/:id/edit', to: "tasks#edit"
+  patch '/tasks/:id', to: "tasks#update"
+
+  #delete a task
+  delete 'tasks/:id', to: "tasks#destroy"
+
 end
