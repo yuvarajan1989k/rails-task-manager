999c6374eabacd81269f10f22976181de5f438f0
yuva k <yuvarajan1989@gmail.com>
2020-10-20 09:04:33 +0800

    finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,17 +8,33 @@ class TasksController < ApplicationController
   end
 
   def new
+    @tasks = Task.new
   end
 
   def create 
+    @tasks = Task.new(task_params)
+    @tasks.save
+    redirect_to task_path(@tasks)
   end
 
   def edit
+    @tasks = Task.find(params[:id])
   end
 
   def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(task_params)
+    redirect_to task_path(@tasks)
   end
 
   def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.delete
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/tasks', to: "tasks#index"
+  #get '/tasks', to: "tasks#index"
+  #get '/tasks/new', to: "tasks#new"
   #read one task
-  get '/tasks/:id', to: "tasks#show"
+  #get '/tasks/:id', to: "tasks#show"
   #create a task
-  get '/tasks/new', to: "tasks#new"
-  post '/tasks', to: "tasks#create"
+ 
+  #post '/tasks', to: "tasks#create"
 
   #update a task
-  get '/tasks/:id/edit', to: "tasks#edit"
-  patch '/tasks/:id', to: "tasks#update"
+  #get '/tasks/:id/edit', to: "tasks#edit"
+  #patch '/tasks/:id', to: "tasks#update"
 
   #delete a task
-  delete 'tasks/:id', to: "tasks#destroy"
+  #delete 'tasks/:id', to: "tasks#destroy"
+
+  resources :tasks
 
 end
